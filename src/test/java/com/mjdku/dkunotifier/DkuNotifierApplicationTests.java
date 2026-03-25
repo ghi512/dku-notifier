f909b493c03c0fb36fcdad73f4237cfa502d4841
김민지 <ghi512151@gmail.com>
2026-03-25 20:37:17 +0900

    init

---
+++ src/test/java/com/mjdku/dkunotifier/DkuNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjdku.dkunotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkuNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
